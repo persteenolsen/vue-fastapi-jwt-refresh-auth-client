5a73f21322319bc7653db6009bd389f6055a9aea
Per Olsen <persteen22@gmail.com>
2026-02-06 14:12:09 +0100

    Tested the build and deployment workflow

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -37,7 +37,7 @@ function onSubmit(values, { setErrors }) {
         <h3>Vue 3 SPA + FastAPI doing Authentication by JWT and Refresh Token Rotation</h3>
         
         <br/><br />
-        <b>Last updated:</b> 28-01-2026
+        <b>Last updated:</b> 06-02-2026
         <br /><br />
 
         <div class="alert alert-info">
